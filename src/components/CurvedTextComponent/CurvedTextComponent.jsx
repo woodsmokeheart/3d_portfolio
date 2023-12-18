c503539a1c2f33cef93b0dc50e94b8bb0a27229e
Denis Kukobin <denis.kukobin@hipasus.com>
2023-12-18 18:27:03 +0300

    redesign Welcome

--- src/components/CurvedTextComponent/CurvedTextComponent.jsx
+++ src/components/CurvedTextComponent/CurvedTextComponent.jsx
@@ -4,14 +4,17 @@ import cn from "classnames";
 
 const CurvedTextComponent = () => {
   return (
-    <div className={css.curvedTextContainer}>
-      <p className={css.letter}>w</p>
-      <p className={cn(css.secondChar, css.letter)}>e</p>
-      <p className={cn(css.threeChar, css.letter)}>l</p>
-      <p className={cn(css.fourChar, css.letter)}>c</p>
-      <p className={cn(css.threeChar, css.letter)}>o</p>
-      <p className={cn(css.secondChar, css.letter)}>m</p>
-      <p className={css.letter}>e</p>
+    <div className={css.wrapper}>
+      <div className={css.curvedTextContainer}>
+        <p className={css.letter}>w</p>
+        <p className={cn(css.secondChar, css.letter)}>e</p>
+        <p className={cn(css.threeChar, css.letter)}>l</p>
+        <p className={cn(css.fourChar, css.letter)}>c</p>
+        <p className={cn(css.threeChar, css.letter)}>o</p>
+        <p className={cn(css.secondChar, css.letter)}>m</p>
+        <p className={css.letter}>e</p>
+      </div>
+      {/* <div className={css.subText}>to my world</div> */}
     </div>
   );
 };
